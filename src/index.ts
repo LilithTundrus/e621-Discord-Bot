14ebcc583b101a22a464153bb2e5b72db4704d38
Lilith Tundrus <echofox123@gmail.com>
2018-05-25 19:40:01 -0400

    added test command

--- src/index.ts
+++ src/index.ts
@@ -52,11 +52,18 @@ client.on('message', async message => {
     const args = message.content.slice(prefix.length).trim().split(/ +/g);
     const command = args.shift().toLowerCase();
 
-    if (command === "ping") {
+    if (command === 'ping') {
         // Calculates ping between sending a message and editing it, giving a nice round-trip latency.
         // The second ping is an average latency between the bot and the websocket server (one-way, not round-trip)
         const m: any = await message.channel.send("Ping?");
         m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
+    }
+
+    if (command === 'test') {
+        return wrapper.posts.getPopularPosts(0)
+        .then((response) => {
+            message.channel.send(response[0].file_url)
+        })
     }
 
 });
