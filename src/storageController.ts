64d89353aa6b04bfe646a714fe765a57eebc8fcf
Lilith Tundrus <echofox123@gmail.com>
2018-05-31 22:53:27 -0400

    renamed readFile fiunction

--- src/storageController.ts
+++ src/storageController.ts
@@ -1,11 +1,25 @@
 import * as fs from 'fs';
 import * as path from 'path';
-
 import { registeredChannelsFile } from './config';
+import * as Discord from 'discord.js';
 
-export function readServerFile() {
+export interface channelInfo {
+    id: number | string;
+}
+
+export function readChannelsFile(): channelInfo[] {
     // read the registeredServers.json file and return contents
     // TODO: Make this async!
     return JSON.parse(fs.readFileSync(registeredChannelsFile, 'UTF-8'));
+}
+
+export function registerChannel(channelID) {
+
+    let workingJSON = readChannelsFile();
+    workingJSON.push({
+        id: channelID
+    });
+    // we should probably make sure the file hasn't changed between being read and written to
+    fs.writeFileSync(registeredChannelsFile, JSON.stringify(workingJSON, null, 2))
 }
 
